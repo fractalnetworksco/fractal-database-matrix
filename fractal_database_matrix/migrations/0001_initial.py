75bdc6e9f4898bd0e28c66eb33fff6b2b1a1b135
Justin <jrussel1055@gmail.com>
2024-07-25 10:30:40 -0500

    fix migrations

--- fractal_database_matrix/migrations/0001_initial.py
+++ fractal_database_matrix/migrations/0001_initial.py
@@ -8,128 +8,71 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ("fractal_database", "0001_initial"),
+        ('fractal_database', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
-            name="MatrixHomeserver",
+            name='MatrixHomeserver',
             fields=[
-                (
-                    "service_ptr",
-                    models.OneToOneField(
-                        auto_created=True,
-                        on_delete=django.db.models.deletion.CASCADE,
-                        parent_link=True,
-                        primary_key=True,
-                        serialize=False,
-                        to="fractal_database.service",
-                    ),
-                ),
-                ("url", models.URLField(unique=True)),
-                (
-                    "priority",
-                    models.PositiveIntegerField(blank=True, default=0, null=True),
-                ),
-                (
-                    "registration_token",
-                    models.CharField(blank=True, max_length=255, null=True),
-                ),
+                ('service_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='fractal_database.service')),
+                ('url', models.URLField(unique=True)),
+                ('priority', models.PositiveIntegerField(blank=True, default=0, null=True)),
+                ('registration_token', models.CharField(blank=True, max_length=255, null=True)),
             ],
             options={
-                "abstract": False,
+                'abstract': False,
             },
-            bases=("fractal_database.service",),
+            bases=('fractal_database.service',),
         ),
         migrations.CreateModel(
-            name="MatrixCredentials",
+            name='MatrixCredentials',
             fields=[
-                (
-                    "id",
-                    models.UUIDField(
-                        default=uuid.uuid4,
-                        editable=False,
-                        primary_key=True,
-                        serialize=False,
-                    ),
-                ),
-                ("date_created", models.DateTimeField(auto_now_add=True)),
-                ("date_modified", models.DateTimeField(auto_now=True)),
-                ("deleted", models.BooleanField(default=False)),
-                ("matrix_id", models.CharField(max_length=255)),
-                ("password", models.CharField(blank=True, max_length=255, null=True)),
-                ("access_token", models.CharField(max_length=255)),
-                (
-                    "device",
-                    models.ForeignKey(
-                        on_delete=django.db.models.deletion.CASCADE,
-                        to="fractal_database.device",
-                    ),
-                ),
-                (
-                    "homeserver",
-                    models.ForeignKey(
-                        on_delete=django.db.models.deletion.CASCADE,
-                        related_name="credentials",
-                        to="fractal_database_matrix.matrixhomeserver",
-                    ),
-                ),
+                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
+                ('date_created', models.DateTimeField(auto_now_add=True)),
+                ('date_modified', models.DateTimeField(auto_now=True)),
+                ('deleted', models.BooleanField(default=False)),
+                ('matrix_id', models.CharField(max_length=255)),
+                ('password', models.CharField(blank=True, max_length=255, null=True)),
+                ('access_token', models.CharField(max_length=255)),
+                ('device', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='fractal_database.device')),
+                ('homeserver', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='credentials', to='fractal_database_matrix.matrixhomeserver')),
             ],
             options={
-                "abstract": False,
+                'abstract': False,
             },
         ),
         migrations.CreateModel(
-            name="InMemoryMatrixCredentials",
-            fields=[],
-            options={
-                "proxy": True,
-                "indexes": [],
-                "constraints": [],
-            },
-            bases=("fractal_database_matrix.matrixcredentials",),
-        ),
-        migrations.CreateModel(
-            name="MatrixReplicationChannel",
+            name='InMemoryMatrixCredentials',
             fields=[
-                (
-                    "id",
-                    models.UUIDField(
-                        default=uuid.uuid4,
-                        editable=False,
-                        primary_key=True,
-                        serialize=False,
-                    ),
-                ),
-                ("date_created", models.DateTimeField(auto_now_add=True)),
-                ("date_modified", models.DateTimeField(auto_now=True)),
-                ("deleted", models.BooleanField(default=False)),
-                ("object_version", models.PositiveIntegerField(default=0)),
-                ("name", models.CharField(max_length=255)),
-                ("enabled", models.BooleanField(default=True)),
-                ("filter", models.CharField(blank=True, max_length=255, null=True)),
-                ("target", models.BooleanField(default=True)),
-                ("source", models.BooleanField(default=True)),
-                ("metadata", models.JSONField(default=dict)),
-                ("database_type", models.CharField(max_length=255)),
-                (
-                    "database",
-                    models.ForeignKey(
-                        on_delete=django.db.models.deletion.CASCADE,
-                        to="fractal_database.database",
-                    ),
-                ),
-                (
-                    "homeserver",
-                    models.ForeignKey(
-                        on_delete=django.db.models.deletion.CASCADE,
-                        related_name="channels",
-                        to="fractal_database_matrix.matrixhomeserver",
-                    ),
-                ),
             ],
             options={
-                "abstract": False,
+                'proxy': True,
+                'indexes': [],
+                'constraints': [],
+            },
+            bases=('fractal_database_matrix.matrixcredentials',),
+        ),
+        migrations.CreateModel(
+            name='MatrixReplicationChannel',
+            fields=[
+                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
+                ('date_created', models.DateTimeField(auto_now_add=True)),
+                ('date_modified', models.DateTimeField(auto_now=True)),
+                ('deleted', models.BooleanField(default=False)),
+                ('object_version', models.PositiveIntegerField(default=0)),
+                ('name', models.CharField(max_length=255)),
+                ('enabled', models.BooleanField(default=True)),
+                ('filter', models.CharField(blank=True, max_length=255, null=True)),
+                ('target', models.BooleanField(default=True)),
+                ('source', models.BooleanField(default=True)),
+                ('metadata', models.JSONField(default=dict)),
+                ('database_type', models.CharField(max_length=255)),
+                ('database', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='fractal_database.database')),
+                ('homeserver', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='channels', to='fractal_database_matrix.matrixhomeserver')),
+            ],
+            options={
+                'abstract': False,
             },
         ),
     ]
